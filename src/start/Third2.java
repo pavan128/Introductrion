4c55a4b4d176224ee1166048d1b63f7472a8cb02
Pavan Kumar Reddy <Pavan Kumar Reddy@DESKTOP-5D4VB24>
2020-03-31 11:07:52 +0530

    commit to pavan branch.....

---
+++ src/start/Third2.java
@@ -0,0 +1,18 @@
+package start;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class Third2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.ie.driver","E:\\Pavan\\IEDriverServer.exe");
+		WebDriver driver= new InternetExplorerDriver();
+		driver.get("http://google.com");
+		System.out.println(driver.getTitle());
+
+	}
+
+}
